d8f99a0487a9fa0930b09ee1a2d808991a846496
Harkamaljit Singh <harkamal_guraya@hotmail.com>
2018-10-17 13:11:32 +0200

    Finite State Machine

--- fsm.py
+++ fsm.py
@@ -5,17 +5,20 @@ class FiniteStateMachine:
     # Agent is a KPC object
     def __init__(self, agent):
         self.agent = agent
+        self.rules = []
 
     # add a new rule to the end of the FSM's rule list.
-    def add_rule(self):
-        pass
+    def add_rule(self, rule):
+        self.rules.append(rule)
 
     # query the agent for the next signal.
     def get_next_signal(self):
+        # next_signal = self.agent.get_next_signal()
         pass
 
     
     def run_rules(self):
+
         pass
 
     def apply_rule(self):
@@ -26,3 +29,11 @@ class FiniteStateMachine:
 
     def main_loop(self):
         pass
+
+# Rule object to use in the FiniteStateMachine class
+class Rule:
+    def __init__(self):
+        self.trig_state = None
+        self.new_state = None
+        self.trig_signal = None
+        self.action = None
